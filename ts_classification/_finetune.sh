e652457069d1e9def21fac179e96c2040830cd2d
keanepotato <keane.ongweiyang@u.nus.edu>
2026-02-27 03:38:41 -0500

    sync latest

--- ts_classification/_finetune.sh
+++ ts_classification/_finetune.sh
@@ -4,21 +4,22 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"  # ts_classification/
 PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"              # SmellNet/
 DATA_ROOT="${PROJECT_ROOT}/smell_ts_dataset/SmellNet"
-DEFAULT_CLASSES=("apple" "banana" "asparagus" "avocado")
+# DEFAULT_CLASSES=("apple" "banana" "asparagus" "avocado")
+DEFAULT_CLASSES="all"
 DEFAULT_FEATURES=("NO2" "C2H5OH" "VOC" "CO" "Alcohol" "LPG" "Benzene")
 
 WANDB_PROJECT="smell-net"
-WANDB_RUN_NAME=""   # leave empty to let WandB auto-generate a name
+WANDB_RUN_NAME="all_test"   # leave empty to let WandB auto-generate a name
 
 python "${SCRIPT_DIR}/_finetune_run.py" \
   --mode train \
   --data-root "${DATA_ROOT}" \
   --classes "${DEFAULT_CLASSES[@]}" \
   --features "${DEFAULT_FEATURES[@]}" \
-  --model "tscmamba" \
+  --model "timesnet" \
   --seq-len 512 \
   --batch-size 2 \
-  --epochs 30 \
+  --epochs 7 \
   --learning-rate 1e-3 \
   --save-dir "${PROJECT_ROOT}/smell_model_ckpts" \
   --wandb-project "${WANDB_PROJECT}" \
